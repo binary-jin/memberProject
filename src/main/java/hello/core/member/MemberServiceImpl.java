cea16985bca58a567d8cd52257fb17dddaba5730
yi-jin <dlwls4338@naver.com>
2023-06-01 15:27:21 +0900

    OrderService, discountPolicy 완성 및 AppConfig를 통해 의존성 주입

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -2,7 +2,11 @@ package hello.core.member;
 
 public class MemberServiceImpl implements MemberService{
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     @Override
     public void join(Member member) {
@@ -14,3 +18,7 @@ public class MemberServiceImpl implements MemberService{
         return memberRepository.findById(memberId);
     }
 }
+//MemberServiceImpl은 MemoryMemberRepository를 의존하지 않고 MemberRepository 인터페이스만 의존함
+//MemberServiceImpl의 입장에서 생성자를 통해 어떤 구현 객체가 들어올 지는 알 수 없음
+//MemberServiceImpl의 생성자를 통해 어떤 구현 객체가 들어올지는 오직 외부(AppConfig)에서만 결정 됨
+//MemberServiceImpl은 이제부터 의존관계에 대한 고민은 외부에 맡기고 실행에만 집중하면 됨
---
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -0,0 +1,41 @@
+package hello.core.order;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.Member;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+
+public class OrderServiceImpl implements OrderService{
+
+    //private final MemberRepository memberRepository = new MemoryMemberRepository();
+//    //private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+//    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+    /*orderServiceImpl은 OrderService에 관련된 로직만 해야하는데
+    discountPolicy를 자기가 직접 FixDiscountpolicy를 할지
+    RateDiscountPolicy를 할지 OrderServiceImpl이 선택, 객체를 만드는 코드임*/
+    /*위의 상황을 방지하기 위해 구현 객체를 생성하고 연결하는 별도의 객체를 만들자!*/
+
+    private final MemberRepository memberRepository;
+    private final DiscountPolicy discountPolicy;
+
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
+
+    @Override
+    public Order createOrder(Long memberId, String itemName, int itemPrice) {
+
+        Member member = memberRepository.findById(memberId);
+        int discountPrice = discountPolicy.discount(member,itemPrice);
+
+        return new Order(memberId, itemName, itemPrice, discountPrice);
+
+    }
+    //OrderServiceImpl은 FixDiscountPolicy를 의존하지 않고 DiscountPolicy 인터페이스만 의존함
+    //OrderServiceImpl 입장에서 생성자를 통해 어떤 구현 객체가 들어올지는 알 수 없음
+    //OrderServiceImpl은 생성자를 통해 어떤 구현 객체를 주입할지는 오직 외부(AppConfig)에서만 결정함
+    //OrderServiceImpl은 실행에만 집중하면 됨
+}
